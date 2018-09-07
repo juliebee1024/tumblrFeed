c7d1fb13e25a7e0b45ab13e34107ef4ef46a039d
Julie Bao <juliebao@Julies-MacBook-Pro.local>
2018-09-06 23:45:21 -0700

    images load but are sized weird

--- tumblrFeed/ViewControllers/PhotosViewController.swift
+++ tumblrFeed/ViewControllers/PhotosViewController.swift
@@ -19,6 +19,9 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -29,7 +32,6 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
             } else if let data = data,
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                 print(dataDictionary)
-                
                 // TODO: Get the posts and store in posts property
                 // Get the dictionary from the response key
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
@@ -48,6 +50,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count //so that # of rows in table = # of posts
     }
@@ -55,6 +58,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
         let post = posts[indexPath.row] //pulls a single post from our posts array
+
         // 1.            // 2.          // 3.
         if let photos = post["photos"] as? [[String: Any]] {
             // photos is NOT nil, we can use it!
